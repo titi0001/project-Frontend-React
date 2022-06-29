aeba7d74df78a0c0a5637bb3c9d1e579135b1443
Thiago Pereira <titiura@gmail.com>
2022-06-29 20:07:35 -0300

    Requisito 8- Renderize o componente Title dentro do componente Missions- ok

--- src/components/Title.js
+++ src/components/Title.js
@@ -4,7 +4,6 @@ import PropTypes from 'prop-types';
 class Title extends React.Component {
   render() {
     const { headline } = this.props;
-    console.log(headline);
     return (
       <h2 className="title">{ headline }</h2>
     );
